6e4765ea64eee7e36c060f5caf0e0013bc95b963
kosukeobata <kosuke.obata@bizreach.co.jp>
2016-12-17 16:10:40 +0900

    Initial Commit.

---
+++ db/migrate/20161217070616_create_introductions.rb
@@ -0,0 +1,10 @@
+class CreateIntroductions < ActiveRecord::Migration
+  def change
+    create_table :introductions do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
